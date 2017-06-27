eb511f12a950c77caff5a21f9a88cf40d37689cc
aPinat <apinat.dowrueng@outlook.de>
2017-06-28 00:02:12 +0700

    new

--- app.py
+++ app.py
@@ -1,69 +1,36 @@
-#import card_random
 import random
-import itertools
-stake = input("How much money would you like to put in: ")
 
-print("You receive: ")
+def cardname(cardval):
+    global points
+    suit = random.choice([' of Clubs', ' of Diamonds', ' of Hearts', ' of Spades'])
+    if cardval == 1:
+        print("Ace" + suit)
+        pointsace = input("ACE! Enter 1 or 11 points?")
+        if pointsace == 1:
+            points += pointsace
+        elif pointsace == 11:
+            points += pointsace
+        else:
+            print("DO NOT CHEAT!")
+    elif cardval < 11:
+        print(str(cardval) + suit)
+        points += cardval
+    elif cardval == 11:
+        print("Jack" + suit)
+        points += 10
+    elif cardval == 12:
+        print("Queen" + suit)
+        points += 10
+    elif cardval == 13:
+        print("King" + suit)
+        points += 10
+    return
 
-cardval = random.randint(1, 13)
+card1 = random.randint(1, 13)
+points = 0
+print("1. Card:")
+cardname(card1)
+print("Current Points: " + str(points))
 
-choose cardval:
-    case 1:
-        rank = "Ace of"
-    case 2:
-        rank = "2 of"
-        points = 2
-    case 3:
-        rank = "3 of"
-        #points....
-    case 4:
-        rank = "4 of"
-    case 5:
-        rank = "5 of"    
-    case 6:
-        rank = "6 of"
-    case 7:
-        rank = "7 of"
-    case 8:
-        rank = "8 of"
-    case 9:
-        rank = "9 of"
-    case 10:
-        rank = "10 of"
-    case 11:
-        rank = "Jack of"
-    case 12:
-        rank = "Queen of"
-    case 13:
-        rank = "King of"
-
-suit = random.choice((' Clubs',' Diamonds',' Hearts',' Spades'))
-card = rank + suit
-
-print(card)
-
-if cardval = 1:
-    ace = input("Would you like 1 or 11 points: ")
-    choose ace:
-        case 1:
-            print("Ace will count as 1")
-            points = 1
-            break
-        case 11:
-            print("Ace will count as 11")
-            points = 11
-            break
-        default:
-            print("ERROR")
-            #Musst du noch weiter ausfuehren
-
-
-rank1 = random.choice(('Ace of','2 of','3 of','4 of','5 of','6 of','7 of','8 of','9 of','10 of','Jack of','Queen of','King of'))
-#suit1 = random.choice((' Clubs',' Diamonds',' Hearts',' Spades'))
-card1 = rank1 #+ suit1
-yesno = input ("Do you want to draw another one? ")
-if yesno =="Yes":
-    print("You receive:")
-    print(card1)
-#versuche grad nen weg zu finden um die karten als zahlen zu definieren
-#print(sum(card))
+newcard = input("Draw another one? (y/n)")
+#So...
